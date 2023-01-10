6cc2f81ca06ef635449f9d342ffa976c6f7b2721
Jason Snowflaketest <122325560+jason-snowflaketest@users.noreply.github.com>
2023-01-10 14:57:33 -0800

    Update streamlit_app.py
    adding header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit
 
 streamlit.title('My Parents\' New Healthy Diner')
+streamlit.header('Menu')
 streamlit.text('Hello world')
 streamlit.text('Bacon & eggs') 
 streamlit.text('pancackes')
