7d4410d90a2e06142302ba5b006f3db445e59773
Jason Snowflaketest <122325560+jason-snowflaketest@users.noreply.github.com>
2023-01-11 15:27:54 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,3 @@
 import streamlit
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+import pandas
 
